536e1f4002201ef4caef99397b9d2a1fc9e8f879
joseluisvaz <joseluisv1201@gmail.com>
2024-10-27 14:15:01 -0700

    waymo dataset converter

---
+++ waymo_loader/feature_description.py
@@ -0,0 +1,137 @@
+
+from typing import Dict
+
+import tensorflow as tf
+
+_ROADGRAPH_SAMPLE_TYPE_MAP = {
+    "LaneCenter-Freeway": 1,
+    "LaneCenter-SurfaceStreet": 2,
+    "LaneCenter-BikeLane": 3,
+    "RoadLine-BrokenSingleWhite": 6,
+    "RoadLine-SolidSingleWhite": 7,
+    "RoadLine-SolidDoubleWhite": 8,
+    "RoadLine-BrokenSingleYellow": 9,
+    "RoadLine-BrokenDoubleYellow": 10,
+    "Roadline-SolidSingleYellow": 11,
+    "Roadline-SolidDoubleYellow": 12,
+    "RoadLine-PassingDoubleYellow": 13,
+    "RoadEdgeBoundary": 15,
+    "RoadEdgeMedian": 16,
+    "StopSign": 17,
+    "Crosswalk": 18,
+    "SpeedBump": 19,
+}
+
+# Use this map to map the int elements of the roadgraph maps to a sequence of contiguous ints.
+_ROADGRAPH_SAMPLE_TYPE_MAP_RESAMPLED = {v: i for i, (_, v) in enumerate(_ROADGRAPH_SAMPLE_TYPE_MAP.items())}
+
+_TL_STATUS_MAP = {
+    "Unknown": 0,
+    "Arrow_Stop": 1,
+    "Arrow_Caution": 2,
+    "Arrow_Go": 3,
+    "Stop": 4,
+    "Caution": 5,
+    "Go": 6,
+    "Flashing_Stop": 7,
+    "Flashing_Caution": 8,
+}
+
+WAYMO_MAP_FEATURE_TO_COLOR = {
+    0: "gray",
+    1: "red",
+    2: "yellow",
+    3: "green",
+    4: "red",
+    5: "yellow",
+    6: "green",
+    7: "red",
+    8: "yellow",
+}
+
+WAYMO_AGENT_TO_COLOR = {
+    0: "gray", # unset
+    1: "teal", # vehicle
+    2: "pink", # pedestrian
+    3: "black", # other
+}
+
+
+def get_feature_description() -> Dict:
+
+    # Example field definition
+    roadgraph_features = {
+        "roadgraph_samples/dir": tf.io.FixedLenFeature([20000, 3], tf.float32, default_value=None),
+        "roadgraph_samples/id": tf.io.FixedLenFeature([20000, 1], tf.int64, default_value=None),
+        "roadgraph_samples/type": tf.io.FixedLenFeature([20000, 1], tf.int64, default_value=None),
+        "roadgraph_samples/valid": tf.io.FixedLenFeature([20000, 1], tf.int64, default_value=None),
+        "roadgraph_samples/xyz": tf.io.FixedLenFeature([20000, 3], tf.float32, default_value=None),
+    }
+
+    # Features of other agents.
+    state_features = {
+        "state/id": tf.io.FixedLenFeature([128], tf.float32, default_value=None),
+        "state/type": tf.io.FixedLenFeature([128], tf.float32, default_value=None),
+        "state/is_sdc": tf.io.FixedLenFeature([128], tf.int64, default_value=None),
+        "state/tracks_to_predict": tf.io.FixedLenFeature([128], tf.int64, default_value=None),
+        # "state/objects_of_interest": tf.io.FixedLenFeature([128], tf.int64, default_value=None),
+        "state/current/bbox_yaw": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/height": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/length": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/timestamp_micros": tf.io.FixedLenFeature([128, 1], tf.int64, default_value=None),
+        "state/current/valid": tf.io.FixedLenFeature([128, 1], tf.int64, default_value=None),
+        "state/current/vel_yaw": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/velocity_x": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/velocity_y": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/speed": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/width": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/x": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/y": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/current/z": tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
+        "state/future/bbox_yaw": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/height": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/length": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/timestamp_micros": tf.io.FixedLenFeature([128, 80], tf.int64, default_value=None),
+        "state/future/valid": tf.io.FixedLenFeature([128, 80], tf.int64, default_value=None),
+        "state/future/vel_yaw": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/velocity_x": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/velocity_y": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/speed": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/width": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/x": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/y": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/future/z": tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
+        "state/past/bbox_yaw": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/height": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/length": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/timestamp_micros": tf.io.FixedLenFeature([128, 10], tf.int64, default_value=None),
+        "state/past/valid": tf.io.FixedLenFeature([128, 10], tf.int64, default_value=None),
+        "state/past/vel_yaw": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/velocity_x": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/velocity_y": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/speed": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/width": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/x": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/y": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+        "state/past/z": tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
+    }
+
+    traffic_light_features = {
+        "traffic_light_state/current/state": tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
+        "traffic_light_state/current/valid": tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
+        "traffic_light_state/current/x": tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
+        "traffic_light_state/current/y": tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
+        "traffic_light_state/current/z": tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
+        "traffic_light_state/current/id": tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
+        "traffic_light_state/past/state": tf.io.FixedLenFeature([10, 16], tf.int64, default_value=None),
+        "traffic_light_state/past/valid": tf.io.FixedLenFeature([10, 16], tf.int64, default_value=None),
+        "traffic_light_state/past/x": tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
+        "traffic_light_state/past/y": tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
+        "traffic_light_state/past/z": tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
+    }
+
+    features_description = {}
+    features_description.update(roadgraph_features)
+    features_description.update(state_features)
+    features_description.update(traffic_light_features)
+    return features_description
---
+++ waymo_loader/waymo_converter.py
@@ -0,0 +1,99 @@
+import pathlib
+from argparse import ArgumentParser, Namespace
+import typing as T
+
+import h5py
+import numpy as np
+import tensorflow as tf
+import multiprocessing as mp
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+
+# from utils.datasets.waymo import WaymoDataset, WaymoDatasetHelper
+from feature_description import get_feature_description
+from torch.utils.data import DataLoader, Dataset, IterableDataset
+
+VALIDATION_LENGTH = 44102
+BATCH_SIZE = 200  # Adjust the batch size as needed
+
+def _parse_arguments() -> Namespace:
+    parser = ArgumentParser(allow_abbrev=True)
+    parser.add_argument("--data-dir", type=str, required=True, help="Path to the folder with the tf records.")
+    parser.add_argument("--out", type=str, required=True, help="Output h5 file")
+    return parser.parse_args()
+
+def _generate_records_from_files(files: T.List[str]) -> T.Generator[T.Dict[str, np.ndarray], None, None]:
+    """Generates the records from the files."""
+    dataset = tf.data.TFRecordDataset(files, compression_type="")
+    for payload in dataset.as_numpy_iterator():
+        sample = tf.io.parse_single_example(payload, get_feature_description())
+        yield {k: v.numpy() for k, v in sample.items()}
+
+def _create_datasets(file: h5py.File, new_data: T.Dict[str, np.ndarray]) -> None:
+    """Create a dataset in the h5 file for every field in the new data sample."""
+    for feature_name, tensor in new_data.items():
+        tensor = np.expand_dims(tensor, axis=0)
+        maxshape = (None, *tensor.shape[1:])
+        file.create_dataset(feature_name, data=tensor, chunks=True, maxshape=maxshape)
+
+def _append_to_datasets(file: h5py.File, batch_data: T.List[T.Dict[str, np.ndarray]]) -> None:
+    """Append a batch of new data samples to the h5 file."""
+    for feature_name in batch_data[0].keys():
+        tensors = np.concatenate([np.expand_dims(data[feature_name], axis=0) for data in batch_data], axis=0)
+        number_of_elements = file[feature_name].shape[0]
+        file[feature_name].resize(number_of_elements + tensors.shape[0], axis=0)
+        file[feature_name][-tensors.shape[0]:] = tensors
+
+def _get_file_list(data_dir: str) -> T.List[str]:
+    """Get file list from a data path."""
+    assert pathlib.Path(data_dir).is_dir()
+    return [str(file) for file in pathlib.Path(data_dir).absolute().rglob("*") if file.is_file()]
+
+def _process_files(file_paths: T.List[str], queue: mp.Queue, progress_bar: tqdm) -> None:
+    """Process files and put the results in a queue."""
+    for record in _generate_records_from_files(file_paths):
+        queue.put(record)
+        
+        progress_bar.update(1) # Update the progress bar in the secondary process 
+
+    queue.put(None)  # Signal that processing is done
+
+def _convert_to_h5(data_dir: str, out_path: str) -> None:
+    """Convert the Waymo dataset into an h5 file that we can use for training."""
+    file_paths = _get_file_list(data_dir)
+    queue = mp.Queue(maxsize=10 * BATCH_SIZE)
+    with tqdm(total=VALIDATION_LENGTH, desc="Processing files") as progress_bar:
+        process = mp.Process(target=_process_files, args=(file_paths, queue, progress_bar))
+        process.start()
+
+        with h5py.File(out_path, "w") as f, ThreadPoolExecutor(max_workers=1) as executor:
+            batch_data = []
+            future = None
+
+            while True:
+                item = queue.get()
+                if item is None:
+                    break
+
+                batch_data.append(item)
+
+                if len(batch_data) == 1:
+                    _create_datasets(f, item)
+
+                if len(batch_data) >= BATCH_SIZE:
+                    if future:
+                        future.result()  # Wait for the previous batch to be processed
+                    future = executor.submit(_append_to_datasets, f, batch_data)
+                    batch_data = []  # Reset the batch data
+
+            if batch_data:
+                if future:
+                    future.result()  # Ensure the previous batch is processed
+                _append_to_datasets(f, batch_data)
+
+        process.join()
+    
+
+if __name__ == "__main__":
+    args = _parse_arguments()
+    _convert_to_h5(args.data_dir, args.out)
